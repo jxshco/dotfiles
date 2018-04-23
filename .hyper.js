3e164d32692d33ca801c6abc8590ef9873b4ee97
Josh Sturrock <jxshco@gmail.com>
2018-04-23 08:33:46 +0100

    Changed to zsh

--- .hyper.js
+++ .hyper.js
@@ -31,7 +31,7 @@ module.exports = {
       lightCyan: '#00ffff',
       lightWhite: '#ffffff'
     },
-    shell: '/bin/bash',
+    shell: '/usr/local/bin/zsh',
     shellArgs: ['--login'],
     env: {},
     bell: false,
