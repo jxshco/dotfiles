705346b6b420360854d373f3e52447bbdf9a2d42
Josh Sturrock <jxshco@gmail.com>
2018-05-11 11:23:18 +0100

    ZSH Updates Added Files for Config Removed OhMyZSH Added starter Aliases

---
+++ brew.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+brew update
+brew upgrade
+brew install coreutils
+brew install findutils
+
+#INSTALL ZSH
+#CHANGE SHELL
--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 ln -s ~/dotfiles/.hyper.js ~/.hyper.js
 ln -s ~/dotfiles/.nvmrc ~/.nvmrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.aliases ~/.aliases
+ln -s ~/dotfiles/.functions ~/.functions
