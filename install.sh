28476a9e64211ebbacb46e6dd8ca1cc81abac84c
Josh Sturrock <jxshco@gmail.com>
2018-08-17 12:39:10 +0100

    Added Hyper to Install

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/.nvmrc ~/.nvmrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.aliases ~/.aliases
 ln -s ~/dotfiles/.functions ~/.functions
+ln -s ~/dotfiles/.hyper.js ~/.hyper.js
