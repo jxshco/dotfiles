4a8308d121b176f59e6e75cd9dfc33626cbac185
Josh Sturrock <jxshco@gmail.com>
2020-02-22 19:22:56 +0000

    Removed hyper.js symlink

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ ln -s ~/dotfiles/.nvmrc ~/.nvmrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.aliases ~/.aliases
 ln -s ~/dotfiles/.functions ~/.functions
-ln -s ~/dotfiles/.hyper.js ~/.hyper.js
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
